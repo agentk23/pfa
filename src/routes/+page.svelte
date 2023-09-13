379805d69910c993ee0d634d5e515dd604278450
agentk23 <dogaru.costin.169@gmail.com>
2023-09-13 13:12:21 +0300

    project skelly

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,44 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import {
+        exists,
+        BaseDirectory,
+        createDir,
+        writeFile,
+        readTextFile
+    } from "@tauri-apps/api/fs";
+
+    async function checkConfig() {
+        let pepe = await exists("config/pepe.txt", {
+            dir: BaseDirectory.AppLocalData,
+        });
+        return pepe;
+    }
+    async function createFile() {
+        await createDir("config", { dir: BaseDirectory.AppLocalData });
+        await writeFile(
+            { path: "config/pepe.txt", contents: "pepe" },
+            { dir: BaseDirectory.AppLocalData }
+        );
+        return "file created";
+    }
+    async function readFile(){
+        let pepe = await readTextFile("config/pepe.txt", {
+            dir: BaseDirectory.AppLocalData,
+        });
+        return pepe;
+    }
+
+    checkConfig().then((res) => {
+        if (!res) {
+            createFile().then((res) => {
+                console.log(res);
+            });
+        } else {
+            readFile().then((res) => {
+                console.log(res);
+            });
+        }
+    });
+</script>
+
+<h1 class="text-center">Welcome to SvelteKit</h1>
